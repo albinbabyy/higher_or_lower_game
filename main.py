f188fde727d486ee6f17f160f64e56c5437db5b5
Albin Baby <albinbaby150@gmail.com>
2022-06-10 15:35:38 +0200

    checking data and user guess correct

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def format_data(account):
     account_country = account["country"]
     return f"{account_name}, {account_descr}, {account_country}"
 
+
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and followers counts and return if they got right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
 # Creating a random account from the game data
 
 account_a = random.choice(data)
@@ -19,3 +28,12 @@ if account_a == account_b:
 
 print(f"comapare A: {format_data(account_a)}")
 print(f"comapare B: {format_data(account_b)}")
+
+guess = input("Who has more followers \Type 'a' or 'b' : \n")
+
+# followers count of each account
+
+a_followers_count = account_a["follower_count"]
+b_followers_count = account_b["follower_count"]
+# check if the user is correct
+is_correct = check_answer(guess , a_followers_count, b_followers_count)

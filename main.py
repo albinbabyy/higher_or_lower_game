5c114d1483f4bf77b1902a742b1db04bd94499ee
Albin Baby <albinbaby150@gmail.com>
2022-06-10 15:50:22 +0200

    lopp defined to keep game on

--- main.py
+++ main.py
@@ -18,29 +18,34 @@ def check_answer(guess, a_followers, b_followers):
         return guess == "b"
 
 score = 0
+
+# for game repeating
+game_should_continue = True
+while game_should_continue:
 # Creating a random account from the game data
 
-account_a = random.choice(data)
-account_b = random.choice(data)
-# in any case account a and b become equal to solve that,
-if account_a == account_b:
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    # in any case account a and b become equal to solve that,
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-print(f"comapare A: {format_data(account_a)}")
-print(f"comapare B: {format_data(account_b)}")
+    print(f"comapare A: {format_data(account_a)}")
+    print(f"comapare B: {format_data(account_b)}")
 
-guess = input("Who has more followers \Type 'a' or 'b' : \n")
+    guess = input("Who has more followers \Type 'a' or 'b' : \n")
 
-# followers count of each account
+    # followers count of each account
 
-a_followers_count = account_a["follower_count"]
-b_followers_count = account_b["follower_count"]
-# check if the user is correct
-is_correct = check_answer(guess, a_followers_count, b_followers_count)
+    a_followers_count = account_a["follower_count"]
+    b_followers_count = account_b["follower_count"]
+    # check if the user is correct
+    is_correct = check_answer(guess, a_followers_count, b_followers_count)
 
-# give feed back on their guess and score keeping
-if is_correct:
-    score +=1
-    print(f"you are right, current score ;{score}")
-else:
-    print(f"sorry, that#s wrong, final score:{score}")
+    # give feed back on their guess and score keeping
+    if is_correct:
+        score +=1
+        print(f"you are right, current score ;{score}")
+    else:
+        game_should_continue = False
+        print(f"sorry, thats wrong, final score:{score}")
